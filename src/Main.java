04a8889f2e04a4dcdc04ef233c1819135c858044
Nikitin Vladislav <nikitin_vladislav1996@mail.ru>
2016-11-15 19:57:00 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Влад on 15.11.2016.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        for(int i=0; i<5; i++)
+        {
+            System.out.println("Число "+ (i+1));
+        }
+    }
+}
