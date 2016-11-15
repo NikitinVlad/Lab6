2930743cd3479ee4a2054e4fc923da5ba7f20aab
Nikitin Vladislav <nikitin_vladislav1996@mail.ru>
2016-11-15 20:58:30 +0300

    5 a lowercase

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Второе изменение");
-        for (int i=10;i>0;i--)
+        System.out.println("Буква а");
+        for(int i=0; i<5;i++)
         {
-            System.out.println("Число "+i);
+            System.out.print("а ");
         }
     }
 }
